7a65ad6f4b7fa73912b68b9e6e42f1fbcc628a7e
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-06-08 14:02:24 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp02/f2.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp02/f2.kt
@@ -2,16 +2,9 @@ package xyz.tberghuis.floatingtimer.tmp.tmp02
 
 import android.app.Application
 import android.content.Context
-import android.content.Intent
-import android.content.pm.ShortcutInfo
 import android.content.pm.ShortcutManager
-import androidx.core.net.toUri
-import android.graphics.drawable.Icon
 import androidx.core.content.getSystemService
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
 import xyz.tberghuis.floatingtimer.R
 import xyz.tberghuis.floatingtimer.data.SavedCountdown
 import xyz.tberghuis.floatingtimer.data.SavedStopwatch
@@ -19,14 +12,10 @@ import xyz.tberghuis.floatingtimer.data.SavedTimer
 import xyz.tberghuis.floatingtimer.data.appDatabase
 import xyz.tberghuis.floatingtimer.data.preferencesRepository
 import xyz.tberghuis.floatingtimer.logd
-import xyz.tberghuis.floatingtimer.viewmodels.toDeepLink
-
 
 fun add_to_homescreen(context: Context, savedTimer: SavedTimer, autoStart: Boolean) {
-
   val shortcut = savedTimer.toShortcutInfo(context, autoStart)
   val shortcutManager = context.getSystemService<ShortcutManager>()
-
   if (shortcutManager?.isRequestPinShortcutSupported == true) {
     shortcutManager.requestPinShortcut(
       shortcut,
@@ -37,14 +26,12 @@ fun add_to_homescreen(context: Context, savedTimer: SavedTimer, autoStart: Boole
 
 suspend fun delete_saved_timer(savedTimer: SavedTimer, application: Application) {
   logd("delete_saved_timer")
-
   val shortcutManager = application.getSystemService<ShortcutManager>()
   val shortcutId = savedTimer.toShortcutId()
   shortcutManager?.disableShortcuts(
     listOf(shortcutId),
     application.getString(R.string.this_timer_has_been_deleted)
   )
-
   when (savedTimer) {
     is SavedStopwatch -> {
       application.appDatabase.savedStopwatchDao().delete(savedTimer)
@@ -54,9 +41,7 @@ suspend fun delete_saved_timer(savedTimer: SavedTimer, application: Application)
       application.appDatabase.savedCountdownDao().delete(savedTimer)
     }
   }
-
 }
-
 
 suspend fun should_Show_Premium_Dialog_Multiple_Timers(fs: FloatingService): Boolean {
   val premiumPurchased =
@@ -64,11 +49,3 @@ suspend fun should_Show_Premium_Dialog_Multiple_Timers(fs: FloatingService): Boo
   val numBubbles = fs.overlayController.getNumberOfBubbles()
   return !premiumPurchased && numBubbles == 2
 }
-
-
-
-
-
-
-
-
