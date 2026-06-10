38665ee264ab55c8d6fe8c8bd622792eebcf0a20
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-06-10 11:24:56 +1000

    .

--- app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp02/DeepLinkActivity.kt
+++ app/src/main/java/xyz/tberghuis/floatingtimer/tmp/tmp02/DeepLinkActivity.kt
@@ -5,43 +5,15 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.LocalActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.lifecycle.viewmodel.compose.viewModel
 import xyz.tberghuis.floatingtimer.INTENT_COMMAND
-import xyz.tberghuis.floatingtimer.INTENT_COMMAND_EXIT
-import xyz.tberghuis.floatingtimer.screens.DeepLinkScreen
-import xyz.tberghuis.floatingtimer.viewmodels.DeepLinkScreenVm
-import xyz.tberghuis.floatingtimer.ui.theme.FloatingTimerTheme
 
 class DeepLinkActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-
-    run {
-      startFloatingServiceForDeeplink(this, intent?.data ?: return@run)
+    intent?.data?.let { data ->
+      startFloatingServiceForDeeplink(this, data)
     }
-
     finishAndRemoveTask()
-
-
-//    enableEdgeToEdge()
-//    setContent {
-//      val vm: DeepLinkScreenVm = viewModel()
-//      val activity = LocalActivity.current
-//      // hack to only run once even if configuration change
-//      rememberSaveable(vm) {
-//        activity?.intent?.data?.let {
-//          vm.processDataUri(it)
-//        }
-//        ""
-//      }
-//      FloatingTimerTheme {
-//        DeepLinkScreen()
-//      }
-//    }
   }
 }
 
@@ -54,4 +26,3 @@ fun startFloatingServiceForDeeplink(context: Context, data: Uri) {
   }
   context.startForegroundService(intent)
 }
-
