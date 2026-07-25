519b8218fd8fbdd422dcdd9214521de315b13dd4
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-07-26 08:07:11 +1000

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
   defaultConfig {
     applicationId = "xyz.tberghuis.floatingtimer"
     minSdk = 29
-    targetSdk = 35
+    targetSdk = 36
     versionCode = 90
     versionName = "1.50.0"
 
